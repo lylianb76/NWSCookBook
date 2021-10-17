4ce3f8b1d37c3fabcb3ce1e1e59c7f5e71e87d92
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2021-10-17 10:19:24 +0200

    Add project files.

---
+++ NWSCookBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NWSCookBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
